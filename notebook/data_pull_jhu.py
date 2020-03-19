3315070f30459cfa610d6de92d65fef61d80a337
sinan <sinkefeli@gmail.com>
2020-03-19 15:40:45 -0700

    updates on the code

--- notebook/data_pull_jhu.py
+++ notebook/data_pull_jhu.py
@@ -1,17 +1,31 @@
+import numpy as np
 import pandas as pd
+import urllib.request
+from datetime import date, timedelta
+import glob
 
-df_countries = pd.read_csv('../input/countries.csv')
-df_countries['Region'] = df_countries['Region'].replace('ASIA', 'Asia')
-df_countries['Region'] = df_countries['Region'].replace('AFRICA', 'Africa')
-df_countries['Region'] = df_countries['Region'].replace('EUROPE', 'Europe')
-df_countries['Region'] = df_countries['Region'].replace('LATIN', 'Latin')
-df_countries['Region'] = df_countries['Region'].replace('NEAR_EAST', 'Near_East')
-df_countries['Region'] = df_countries['Region'].replace('NORTHERN_AMERICA', 'Northern_America')
-df_countries['Region'] = df_countries['Region'].replace('OCEANIA', 'Oceania')
+# download all jhu data
+sdate = date(2020, 1, 22)   # start date
+edate = date.today()   # end date
+delta = edate - sdate
+df_arr = []
+for i in range(delta.days + 1):
+  day = sdate + timedelta(days=i)
+  day = day.strftime('%m-%d-%Y')
+  url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/' + \
+      'csse_covid_19_daily_reports/{:s}.csv'.format(day)
+  fname = '../input/jhu/covid-19_daily_{:s}.csv'.format(day)
+  try:
+    urllib.request.urlretrieve(url, fname)
+    df = pd.read_csv(fname, index_col=None, parse_dates=['Last Update'])
+    df['Date'] = day
+    df_arr.append(df)
+    #print(day)
+  except:
+    continue;
+df_table = pd.concat(df_arr, axis=0, ignore_index=True)
+#df_table['Date'] = pd.DatetimeIndex(df_table['Date']).normalize()
 
-# read covid-19 data, and clean up country names
-df_table = pd.read_csv('../input/covid_19_data.csv', parse_dates=['ObservationDate'])
-df_table.rename(columns={'ObservationDate':'Date'}, inplace=True)
 df_table['Active'] = df_table['Confirmed']-df_table['Deaths']-df_table['Recovered']
 df_table['Closed'] = df_table['Confirmed']-df_table['Active']
 df_table['Country/Region'] = df_table['Country/Region'].replace('Mainland China', 'China')
@@ -38,3 +52,5 @@ df_table['Country/Region'] = df_table['Country/Region'].replace('occupied Palest
 df_table['Country/Region'] = df_table['Country/Region'].replace('Holy See', 'Vatican City')
 df_table['Country/Region'] = df_table['Country/Region'].replace('Czechia', 'Czech Republic')
 df_table['Province/State'] = df_table['Province/State'].fillna('NA')
+
+df_table.to_csv('../input/covid_19_master.csv')
